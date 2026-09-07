16a24ae9d70a792a29a62542440adc3265d839c7
dreaminbb <shinimpact.tnk@gmail.com>
2026-09-08 07:09:51 +0900

    goat

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,13 +3,14 @@ return {
 	lazy = false,
 	dependencies = { "catppuccin/nvim", "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("lualine").setup({
+		require("catppuccin").setup({
+			require("lualine").setup({
 
-			require("catppuccin").setup({
 				options = {
-					theme = "catppuccin",
-					component_separators = {},
-					section_separators = {},
+					theme = "auto",
+					section_separators = { left = "", right = "" },
+					-- セクション内コンポーネントの区切り
+					component_separators = { left = "", right = "" },
 					globalstatus = true,
 
 					disabled_filetypes = {
@@ -17,41 +18,39 @@ return {
 						winbar = { "oil" },
 					},
 				},
+				sections = {
+					lualine_a = {
+						{ "mode" },
+					},
+					lualine_b = {
+						{ "branch", icon = "" },
+						{
+							"diff",
+							symbols = { added = " ", modified = " ", removed = " " },
+						},
+						{
+							"filename",
+							file_status = true,
+							path = 1, -- 1 = Relative path
+							separator = "",
+						},
+						{ "filetype", icon_only = true },
+					},
+					lualine_c = {
+						{
+							"diagnostics",
+							sources = { "nvim_diagnostic" },
+							symbols = { error = " ", warn = " ", info = " ", hint = "<?>" },
+						},
+					},
+
+					lualine_x = {},
+					lualine_y = {
+						{ "encoding" },
+					},
+					lualine_z = { { "progress" } },
+				},
 			}),
-			sections = {
-				lualine_a = {
-					{ "mode" },
-				},
-				lualine_b = {
-					{ "branch", icon = "" },
-					{
-						"diff",
-						symbols = { added = " ", modified = " ", removed = " " },
-					},
-					{
-						"filename",
-						file_status = true,
-						path = 1, -- 1 = Relative path
-						separator = "",
-					},
-					{ "filetype", icon_only = true },
-				},
-				lualine_c = {
-					{
-						"diagnostics",
-						sources = { "nvim_diagnostic" },
-						symbols = { error = " ", warn = " ", info = " ", hint = "<?>" },
-					},
-				},
-
-				lualine_x = {
-
-					{ "encoding" },
-					{ "progress" },
-				},
-				lualine_y = {},
-				lualine_z = {},
-			},
 		})
 	end,
 }
